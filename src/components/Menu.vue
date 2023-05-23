5d0956732836daf7b9f97d184dde4440c88aa46a
loicgalland <loicgalland1@gmail.com>
2023-05-23 10:24:13 +0200

    Création de la page d'accueil

---
+++ src/components/Menu.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="d-flex ma-3">
+    <v-toolbar title="MaBoutique">
+      <v-toolbar-items>
+        <v-btn>
+          <a href="#" class="text-decoration-none"> Home </a>
+        </v-btn>
+      </v-toolbar-items>
+      <v-toolbar-items>
+        <v-btn>
+          <a href="#" class="text-decoration-none color-text-black">
+            Nous contacter
+          </a>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavMenu",
+  data: () => ({
+    items: [{ title: "Home" }, { title: "Nous contacter" }],
+  }),
+};
+</script>
---
+++ src/components/SliderHome.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card elevation="24" max-width="1000" class="mx-auto">
+    <v-carousel hide-delimiters>
+      <v-carousel-item
+        v-for="(image, i) in images"
+        :key="i"
+        :src="image.src"
+        cover
+      ></v-carousel-item>
+    </v-carousel>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "SliderHome",
+  data() {
+    return {
+      images: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
